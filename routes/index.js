1e3e6639ac23d6513aa968e565c0e7479ac18e1d
haeryoung <ok12ok14@naver.com>
2022-12-05 02:35:04 +0900

    edit

--- routes/index.js
+++ routes/index.js
@@ -210,6 +210,11 @@ router.post('/password/edit', async (req, res, next) => {
         userMail:email,
         check: Check,
       });
+    } else if(compare) {
+      res.render("passwordEdit", {
+        userMail:email,
+        check: reCheck,
+      });
     } else {
       const password = await User.update({
         password: hash,
